97c0d85a3c0e6cc9ae94ec3567b3feaa3d5a885e
Denis Gamza <diesel_ru@mail.ru>
2015-07-20 16:50:22 +0800

    billing

--- app/src/main/java/ru/dsoft38/smsinformer/Help.java
+++ app/src/main/java/ru/dsoft38/smsinformer/Help.java
@@ -22,7 +22,7 @@ public class Help extends Activity {
                         "<p><strong>Тема письма:</strong></p>" +
                         "<p>SMSINFORMER <em>(должна совпадать с темой в настройках программы)</em></p>" +
                         "<p><strong>Содержание письма:</strong></p>" +
-                        "<p>&lt;PhoneList&gt;8ХХХХХХХХХХ;+7ХХХХХХХХХХ;8ХХХХХХХХХХ&lt;/PhoneList&gt;<br/>&lt;MessageText&gt;Текст сообщения для СМС. Ограничение 60 символов.&lt;/MessageText&gt;</p>" +
+                        "<p>&lt;PhoneList&gt;8ХХХХХХХХХХ;+7ХХХХХХХХХХ;8ХХХХХХХХХХ&lt;/PhoneList&gt;<br/>&lt;MessageText&gt;Текст сообщения для СМС.&lt;/MessageText&gt;</p>" +
                         "<p><strong>Расшифровка:</strong></p>" +
                         "<ul><li><p>В теге <strong><em>&lt;</em></strong><strong><em>PhoneList&gt;&lt;/</em></strong><strong><em>PhoneList&gt;</em></strong> должен содержаться список\n номеров телефонов получателей, разделенный символом «<strong>;</strong>».</p></li>" +
                         "<li><p>В теге <strong><em>&lt;</em></strong><strong><em>MessageText&gt;&lt;/</em></strong><strong><em>MessageText&gt; </em></strong>должен содержаться\n текст сообщения.</p></li>" +
---
+++ app/src/main/java/ru/dsoft38/smsinformer/InAppBillingActivity.java
@@ -0,0 +1,11 @@
+package ru.dsoft38.smsinformer;
+
+import android.app.Activity;
+
+/**
+ * Created by diesel on 20.07.2015.
+ */
+
+public class InAppBillingActivity extends Activity {
+
+}
