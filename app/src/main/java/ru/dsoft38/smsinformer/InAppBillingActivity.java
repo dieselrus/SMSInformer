4e519c287382ec92112bc0593c1f087f97c2ff13
diesel <diesel_ru@mail.ru>
2015-07-24 00:27:27 +0800

    billeng

--- app/src/main/java/ru/dsoft38/smsinformer/InAppBillingActivity.java
+++ app/src/main/java/ru/dsoft38/smsinformer/InAppBillingActivity.java
@@ -10,10 +10,10 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.android.vending.billing.IInAppBillingService;
@@ -21,12 +21,14 @@ import com.android.vending.billing.IInAppBillingService;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.security.PublicKey;
 import java.util.ArrayList;
 
 import ru.dsoft38.smsinformer.util.IabHelper;
 import ru.dsoft38.smsinformer.util.IabResult;
 import ru.dsoft38.smsinformer.util.Inventory;
 import ru.dsoft38.smsinformer.util.Purchase;
+import ru.dsoft38.smsinformer.util.Security;
 
 /**
  * Created by diesel on 22.07.2015.
@@ -34,6 +36,7 @@ import ru.dsoft38.smsinformer.util.Purchase;
 public class InAppBillingActivity extends Activity {
     // id вашей покупки из админки в Google Play
     static final String inappid = "com.example.buttonclick";
+    static final  String TAG = "InAppBillingActivity";
 
     public static final String BASE64_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7QGiMiAHNPx59pir0bKmJeGB3DQ2BVL3emDFyUZAB9lwnZTMNdsxlmpRR3PhH+VL/zDL0x6bvsk1Ec8m+L26VxNASBF10yKcpbHpYPIqDSQplq46VZrijVVrxuRS/GT+q1WFCRMdth4hIoMIZ4CdoJvkWfhP5TmBGTLqjSrCmrEIuYfNaZKHhAQ5BamC8aiTMQ5kkv/PG6j6UPmb1c0A7SIAHje7Lc3LFy5bOoDhmRV4LfDyRMORyncs69YTL8P2EuJdnrXMWU+QAmiUumTqbfkEw3RaTK5RPDBHqs1gD99pKtVkmZ9Tj/HRfkYFrFJS8lWJP5fC6qt7alM+y2qwEwIDAQAB";
 
@@ -93,7 +96,7 @@ public class InAppBillingActivity extends Activity {
                                 System.out.println("price " + price);
                                 Bundle buyIntentBundle = mService.getBuyIntent(3, getPackageName(), sku,
                                         "inapp",
-                                                "");
+                                                "bGoa+V7g/yqDXvKRqq+JTFn4uQZbPiQJo4pf9RzJ");
                                 PendingIntent pendingIntent = buyIntentBundle
                                         .getParcelable("BUY_INTENT");
                                 startIntentSenderForResult(
@@ -160,6 +163,71 @@ public class InAppBillingActivity extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
+
+        //buyButton = (Button)findViewById(R.id.buyButton);
+        Button clickButton = (Button)findViewById(R.id.clickButton);
+        clickButton.setEnabled(false);
+
+
+        mHelper = new IabHelper(this, BASE64_PUBLIC_KEY);
+
+        mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+
+            public void onIabSetupFinished(IabResult result) {
+                if (!result.isSuccess()) {
+                    Log.d(TAG, "In-app Billing setup failed: " + result);
+                } else {
+                    Log.d(TAG, "In-app Billing is set up OK");
+                }
+
+                mHelper.queryInventoryAsync(
+                        new IabHelper.QueryInventoryFinishedListener() {
+
+                            @Override
+                            public void onQueryInventoryFinished(IabResult result, Inventory inv) {
+                                Log.d("IAB", "Query inventory finished.");
+                                if (result.isFailure()) {
+                                    Log.d("IAB","Failed to query inventory: " + result);
+                                    return;
+                                }
+
+                                Log.d("IAB", "Query inventory was successful.");
+
+                                // Do we have the premium upgrade?
+                                boolean mIsPremium = inv.hasPurchase(Helper.premiumSku);
+                                Purchase p = inv.getPurchase(Helper.premiumSku);
+                                if(p != null)
+                                    Log.d("IAB PURCHASE STATE", IabHelper.getResponseDesc(p.getPurchaseState()));
+                                else
+                                    Log.d("IAB PURCHASE STATE", "Purchase is null");
+
+                                Log.d("IAB", "User is " + (mIsPremium ? "PREMIUM" : "NOT PREMIUM"));
+
+
+                            }
+                        }
+
+                );
+            }
+        });
+
+
+    }
+
+    public static boolean verifyPurchase(String base64PublicKey,
+                                         String signedData, String signature) {
+        if (TextUtils.isEmpty(signedData) ||
+                TextUtils.isEmpty(base64PublicKey) ||
+                TextUtils.isEmpty(signature)) {
+            Log.e(TAG, "Purchase verification failed: missing data.");
+            if (BuildConfig.DEBUG) {
+                return true;
+            }
+            return false;
+        }
+
+        PublicKey key = Security.generatePublicKey(base64PublicKey);
+        return Security.verify(key, signedData, signature);
     }
 
     @Override
