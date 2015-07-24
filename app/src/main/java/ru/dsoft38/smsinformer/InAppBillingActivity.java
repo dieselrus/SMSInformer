5a2bd555b7905b734176166d12df359a53b3fd90
Denis Gamza <diesel_ru@mail.ru>
2015-07-24 13:11:35 +0800

    получаем покупки

--- app/src/main/java/ru/dsoft38/smsinformer/InAppBillingActivity.java
+++ app/src/main/java/ru/dsoft38/smsinformer/InAppBillingActivity.java
@@ -33,9 +33,13 @@ import ru.dsoft38.smsinformer.util.Purchase;
  */
 public class InAppBillingActivity extends Activity {
     // id вашей покупки из админки в Google Play
-    static final String inappid = "com.example.buttonclick";
+    static final String SKU_TEST = "com.example.buttonclick";
+    // id вашей покупки из админки в Google Play
+    //static final String SKU_ADS_DISABLE = "com.example.buttonclick";
 
     public static final String BASE64_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7QGiMiAHNPx59pir0bKmJeGB3DQ2BVL3emDFyUZAB9lwnZTMNdsxlmpRR3PhH+VL/zDL0x6bvsk1Ec8m+L26VxNASBF10yKcpbHpYPIqDSQplq46VZrijVVrxuRS/GT+q1WFCRMdth4hIoMIZ4CdoJvkWfhP5TmBGTLqjSrCmrEIuYfNaZKHhAQ5BamC8aiTMQ5kkv/PG6j6UPmb1c0A7SIAHje7Lc3LFy5bOoDhmRV4LfDyRMORyncs69YTL8P2EuJdnrXMWU+QAmiUumTqbfkEw3RaTK5RPDBHqs1gD99pKtVkmZ9Tj/HRfkYFrFJS8lWJP5fC6qt7alM+y2qwEwIDAQAB";
+
+    private static final String TAG = "InAppBillingActivity";
 
     IInAppBillingService mService;
     ServiceConnection mServiceConn;
@@ -47,6 +51,8 @@ public class InAppBillingActivity extends Activity {
         setContentView(R.layout.license_buy);
 
         mHelper = new IabHelper(this, BASE64_PUBLIC_KEY);
+
+        billingInit();
 
         mServiceConn = new ServiceConnection() {
             @Override
@@ -70,16 +76,16 @@ public class InAppBillingActivity extends Activity {
             @Override
             public void onClick(View v) {
                 ArrayList skuList = new ArrayList();
-                skuList.add(inappid);
+                skuList.add(SKU_TEST);
                 Bundle querySkus = new Bundle();
                 querySkus.putStringArrayList("ITEM_ID_LIST", skuList);
                 Bundle skuDetails;
                 try {
-                    Log.d("myLogs", getPackageName());
+                    Log.d(TAG, getPackageName());
                     skuDetails = mService.getSkuDetails(3, getPackageName(), "inapp", querySkus);
 
                     int response = skuDetails.getInt("RESPONSE_CODE");
-                    Log.d("myLogs", response + "");
+                    Log.d(TAG, response + "");
                     if (response == 0) {
 
                         ArrayList<String> responseList = skuDetails
@@ -89,7 +95,8 @@ public class InAppBillingActivity extends Activity {
                             JSONObject object = new JSONObject(thisResponse);
                             String sku = object.getString("productId");
                             String price = object.getString("price");
-                            if (sku.equals(inappid)) {
+
+                            if (sku.equals(SKU_TEST)) {
                                 System.out.println("price " + price);
                                 Bundle buyIntentBundle = mService.getBuyIntent(3, getPackageName(), sku,
                                         "inapp",
@@ -123,7 +130,8 @@ public class InAppBillingActivity extends Activity {
             if (resultCode == RESULT_OK) {
                 try {
                     JSONObject jo = new JSONObject(purchaseData);
-                    String sku = jo.getString(inappid);
+
+                    String sku = jo.getString(SKU_TEST);
                     Toast.makeText(
                             InAppBillingActivity.this,
                             "You have bought the " + sku
@@ -137,6 +145,93 @@ public class InAppBillingActivity extends Activity {
             }
         }
     }
+
+    private void billingInit() {
+        mHelper = new IabHelper(this, BASE64_PUBLIC_KEY);
+
+        // включаем дебагинг (в релизной версии ОБЯЗАТЕЛЬНО выставьте в false)
+        mHelper.enableDebugLogging(true);
+
+        // инициализируем; запрос асинхронен
+        // будет вызван, когда инициализация завершится
+        mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            public void onIabSetupFinished(IabResult result) {
+                if (!result.isSuccess()) {
+                    return;
+                }
+
+                // чекаем уже купленное
+                mHelper.queryInventoryAsync(mGotInventoryListener);
+            }
+        });
+    }
+
+    // Слушатель для востановителя покупок.
+    IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+
+        public void onQueryInventoryFinished(IabResult result,
+                                             Inventory inventory) {
+            Log.d(TAG, "Query inventory finished.");
+            if (result.isFailure()) {
+                Log.d(TAG, "Failed to query inventory: " + result);
+                return;
+            }
+
+            Log.d(TAG, "Query inventory was successful.");
+
+            /*
+             * Проверяются покупки.
+             * Обратите внимание, что надо проверить каждую покупку, чтобы убедиться, что всё норм!
+             * см. verifyDeveloperPayload().
+             */
+
+            Purchase purchase = inventory.getPurchase(SKU_TEST);
+/*                PreferencesHelper.savePurchase(
+                        context,
+                        PreferencesHelper.Purchase.DISABLE_ADS,
+                        purchase != null && verifyDeveloperPayload(purchase));
+                ads.show(!PreferencesHelper.isAdsDisabled());*/
+
+        }
+    };
+
+    /** Verifies the developer payload of a purchase. */
+    boolean verifyDeveloperPayload(Purchase p) {
+        String payload = p.getDeveloperPayload();
+		/*
+		 * здесь необходимо свою верификацию реализовать Хорошо бы ещё с
+		 * использованием собственного стороннего сервера.
+		 */
+
+        return true;
+    }
+
+    // Прокает, когда покупка завершена
+    IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
+            Log.d(TAG, "Purchase finished: " + result + ", purchase: "
+                    + purchase);
+            if (result.isFailure()) {
+                return;
+            }
+            if (!verifyDeveloperPayload(purchase)) {
+                return;
+            }
+
+            Log.d(TAG, "Purchase successful.");
+
+            if (purchase.getSku().equals(SKU_TEST)) {
+
+                Log.d(TAG, "Purchase for disabling ads done. Congratulating user.");
+                Toast.makeText(getApplicationContext(), "Purchase for disabling ads done.", Toast.LENGTH_SHORT);
+                // сохраняем в настройках, что отключили рекламу
+                //PreferencesHelper.savePurchase(context, PreferencesHelper.Purchase.DISABLE_ADS, true);
+                // отключаем рекламу
+                //ads.show(!PreferencesHelper.isAdsDisabled());
+            }
+
+        }
+    };
 
     @Override
     public void onDestroy() {
